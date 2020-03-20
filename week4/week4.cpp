6a50d377a287e40d9e48efaed1a717f2fdd207b4
桐 孙 <2731881741@qq.com>
2020-03-20 15:05:56 +0800

    1

--- week4/week4.cpp
+++ week4/week4.cpp
@@ -4,10 +4,45 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 using namespace cv;
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat srcMat = imread("C:\\Users\\27318\\Desktop\\大二下网络课程\\数字图像\\第四周\\clip.png", 0);
+	cv::Mat binaryMat, labelMat, statsMat, centrMat;
+	cv::Mat dstMat;
+
+	/*二值化*/
+	threshold(srcMat, dstMat, 0, 200, THRESH_OTSU);
+	imshow("binary", dstMat);
+
+	//Mat kernel = getStructuringElement(MORPH_CROSS, Size(12, 12));
+	//dilate(binaryMat, dstMat, kernel);
+	//imshow("dilate", dstMat);
+
+	int nComp = connectedComponentsWithStats(dstMat,
+		labelMat,
+		statsMat,
+		centrMat,
+		8,
+		CV_32S);
+
+
+	for (int i = 1; i < nComp; i++) {
+		Rect bndbox;
+		bndbox.x = statsMat.at<int>(i, 0);
+		bndbox.y = statsMat.at<int>(i, 1);
+		bndbox.width = statsMat.at<int>(i, 2);
+		bndbox.height = statsMat.at<int>(i, 3);
+		rectangle(dstMat, bndbox, CV_RGB(255, 255, 255), 1, 8, 0);
+	}
+
+	imshow("output", dstMat);
+	nComp--;
+	cout << "个数：" << nComp << endl;
+
+
+	waitKey(0);
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
